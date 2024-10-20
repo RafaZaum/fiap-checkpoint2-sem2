5aeca3cbe281090fe7a73cfe39eda3a93557b958
Rafael Souza <rafinha-lima1@Live.comgit config --global user.name Rafael>
2024-10-20 17:44:07 -0300

    checkpoint

---
+++ src/main/java/br/com/fiap/ecommerce/dtos/ProdutoRequestUpdateDto.java
@@ -0,0 +1,14 @@
+package br.com.fiap.ecommerce.dtos;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoRequestUpdateDto {
+    private String nome;
+    private BigDecimal valor;
+
+}
